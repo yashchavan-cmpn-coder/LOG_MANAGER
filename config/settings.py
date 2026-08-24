1ea7237f585c7aad3373b25f3679eaec1902133a
yashchavan-cmpn-coder <yashchavan-cmpn@atharvacoe.ac.in>
2026-08-24 14:11:30 +0530

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
